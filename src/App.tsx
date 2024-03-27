81828de54ef1d99f81767c0d4d15902f081d93a6
Hari Bharat <bharat.haristudent@gmail.com>
2024-03-27 18:16:07 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import ResponsiveAppBar from './components/Navbar';
+import AccordionExpandDefault from './components/accordon';
+import TemporaryDrawer from './components/collaps-sidebar';
+import LabTabs from './components/tabs';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{fontFamily:'sans-serif'}}>
+    <header style={{position:'sticky'}}>
+    <ResponsiveAppBar/>
+    </header>
+    <div>{/*body*/}
+    <TemporaryDrawer/>
+      <div style={{marginTop:'2em',marginLeft:'20em',width:'55em'}}>
+      <h1 style={{color:'#2e7d32'}}>Organization</h1>
+      <br/>
+    <LabTabs/>
     </div>
+    </div>
+
+    </div>    
+
   );
 }
 
